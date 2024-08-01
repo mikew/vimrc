71682c2783edf1935d99b9da5e6892920ee3566d
Mike Wyatt <wyatt.mike@gmail.com>
2024-07-31 22:41:19 -0300

    formatting

--- src/nvim/lua/features/editor.lua
+++ src/nvim/lua/features/editor.lua
@@ -299,11 +299,11 @@ mod.plugins = {
     },
     opts = {
       on_attach = function(bufnr)
-        local api = require("nvim-tree.api")
+        local api = require('nvim-tree.api')
 
         local function opts(desc)
           return {
-            desc = "nvim-tree: " .. desc,
+            desc = 'nvim-tree: ' .. desc,
             buffer = bufnr,
             noremap = true,
             silent = true,
