e39c33e47bfcc58516dee1af514c429724022cdc
Mike Wyatt <wyatt.mike@gmail.com>
2024-07-31 22:41:14 -0300

    integrate cmp + autopairs

--- src/nvim/lua/features/editor.lua
+++ src/nvim/lua/features/editor.lua
@@ -204,9 +204,9 @@ mod.plugins = {
     config = function(_, opts)
       require('nvim-autopairs').setup(opts)
       -- If you want to automatically add `(` after selecting a function or method
-      -- local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-      -- local cmp = require('cmp')
-      -- cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      local cmp = require('cmp')
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
     end,
   },
 
