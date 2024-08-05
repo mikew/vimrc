ed69a1d58c98d5e11ea5a5d143f2f131c73dc3bb
Mike Wyatt <wyatt.mike@gmail.com>
2024-08-04 21:32:33 -0300

    use vimrc.register_feature

--- src/nvim/init.lua
+++ src/nvim/init.lua
@@ -1,13 +1,22 @@
 require('lazy-rtp')
 
+local vimrc = require('vimrc')
 local feature_editor = require('features.editor')
+vimrc.register_feature('editor')
 local feature_languages = require('features.languages')
+vimrc.register_feature('languages')
 local feature_scm = require('features.scm')
+vimrc.register_feature('scm')
 local feature_lsp = require('features.lsp')
+vimrc.register_feature('lsp')
 local feature_completion = require('features.completion')
+vimrc.register_feature('completion')
 local feature_sidebar = require('features.sidebar')
+vimrc.register_feature('sidebar')
 local feature_grep = require('features.grep')
+vimrc.register_feature('grep')
 local feature_scrollbar = require('features.scrollbar')
+vimrc.register_feature('scrollbar')
 
 local all_spec = {}
 vim.list_extend(all_spec, feature_editor.plugins)
