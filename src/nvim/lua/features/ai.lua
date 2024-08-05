62fbca64f17b30cad893116f5409c6ee43df459e
Mike Wyatt <wyatt.mike@gmail.com>
2024-08-05 11:02:12 -0300

    change copilot mapping

--- src/nvim/lua/features/ai.lua
+++ src/nvim/lua/features/ai.lua
@@ -3,6 +3,13 @@ local mod = {}
 mod.plugins = {
   {
     'github/copilot.vim',
+    init = function()
+      vim.g.copilot_no_tab_map = true
+      vim.keymap.set('i', '<C-CR>', 'copilot#Accept("")', {
+        expr = true,
+        replace_keycodes = false,
+      })
+    end,
     config = function() end,
   },
 }
