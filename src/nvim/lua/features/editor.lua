c8d85e3c49e51b195e5d429366620a93e1170f3f
Mike Wyatt <wyatt.mike@gmail.com>
2024-08-05 20:30:24 -0300

    add lualine

--- src/nvim/lua/features/editor.lua
+++ src/nvim/lua/features/editor.lua
@@ -436,6 +436,37 @@ mod.plugins = {
     cond = not vim.g.vscode,
     opts = {},
   },
+
+  {
+    'nvim-lualine/lualine.nvim',
+    cond = not vim.g.vscode,
+    opts = {
+      options = {
+        component_separators = { left = '|', right = '|' },
+        section_separators = { left = '', right = '' },
+      },
+
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = {
+          'branch',
+          -- {
+          --   'diagnostics',
+          --   symbols = {
+          --     error = symbols.diagnostics.error,
+          --     warn = symbols.diagnostics.warn,
+          --     info = symbols.diagnostics.info,
+          --     hint = symbols.diagnostics.hint,
+          --   },
+          -- },
+        },
+        lualine_c = { 'filename' },
+        lualine_x = { 'filetype' },
+        lualine_y = {},
+        lualine_z = { 'location' },
+      },
+    },
+  },
 }
 
 return mod
