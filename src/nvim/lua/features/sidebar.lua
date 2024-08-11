6f34fd0de3bc5ba842fc7e28aff9bb1b51edc9fb
Mike Wyatt <wyatt.mike@gmail.com>
2024-08-11 14:45:33 -0300

    cleanup

--- src/nvim/lua/features/sidebar.lua
+++ src/nvim/lua/features/sidebar.lua
@@ -210,36 +210,12 @@ mod.plugins = {
       filters = {
         dotfiles = true,
       },
-
-      -- tab = {
-      --   sync = {
-      --     open = true,
-      --     close = true,
-      --   },
-      -- },
     },
     init = function()
       -- disable netrw
       vim.g.loaded_netrw = 1
       vim.g.loaded_netrwPlugin = 1
-
-      -- vim.api.nvim_create_autocmd('VimEnter', {
-      --   desc = 'Open Tree automatically',
-      --   once = true,
-      --   command = 'NvimTreeFindFile | wincmd p',
-      -- })
     end,
-  },
-
-  {
-    'nvim-neo-tree/neo-tree.nvim',
-    opts = {},
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      -- 'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-      'MunifTanjim/nui.nvim',
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
   },
 }
 
