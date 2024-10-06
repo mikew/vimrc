e11e030ff2dec467a623f1592fee7c51f05f517e
Mike Wyatt <wyatt.mike@gmail.com>
2024-10-06 00:23:30 -0300

    tab sizes

--- src/nvim/lua/features/editor.lua
+++ src/nvim/lua/features/editor.lua
@@ -35,6 +35,11 @@ mod.setup = vimrc.make_setup(function(context)
   vim.keymap.set('i', '<D-CR>', '<C-o>o')
 
   if not vim.g.vscode then
+    vim.opt.shiftwidth = 2
+    vim.opt.tabstop = 2
+    vim.opt.softtabstop = 2
+    vim.opt.expandtab = true
+
     vim.opt.termguicolors = true
     vim.opt.cmdheight = 0
 
