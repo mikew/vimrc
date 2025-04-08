4c784c037fdd411d3eb1769b75c610f942140497
Mike Wyatt <wyatt.mike@gmail.com>
2025-04-08 00:59:25 -0300

    feat: Swap to copilot.lua (#36)

--- src/nvim/lua/features/ai.lua
+++ src/nvim/lua/features/ai.lua
@@ -1,4 +1,5 @@
 local vimrc = require('vimrc')
+local symbols = require('symbols')
 
 local mod = {}
 
@@ -9,17 +10,46 @@ mod.setup = vimrc.make_setup(function(context)
   }
 
   feature.plugins = {
+    -- {
+    --   'github/copilot.vim',
+    --   cond = not vim.g.vscode,
+    --   init = function()
+    --     vim.g.copilot_no_tab_map = true
+    --     vim.keymap.set('i', '<C-CR>', 'copilot#Accept("")', {
+    --       expr = true,
+    --       replace_keycodes = false,
+    --     })
+    --   end,
+    --   config = function() end,
+    -- },
+
     {
-      'github/copilot.vim',
+      'zbirenbaum/copilot.lua',
       cond = not vim.g.vscode,
-      init = function()
-        vim.g.copilot_no_tab_map = true
-        vim.keymap.set('i', '<C-CR>', 'copilot#Accept("")', {
-          expr = true,
-          replace_keycodes = false,
-        })
-      end,
-      config = function() end,
+      event = 'InsertEnter',
+      opts = {
+        panel = {
+          enabled = false,
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          keymap = {
+            accept = '<C-CR>',
+            -- accept_word = false,
+            -- accept_line = false,
+            -- next = '<M-]>',
+            -- prev = '<M-[>',
+            -- dismiss = '<C-]>',
+          },
+        },
+        filetypes = {
+          yaml = true,
+          markdown = true,
+          gitcommit = true,
+          gitrebase = false,
+        },
+      },
     },
   }
 
