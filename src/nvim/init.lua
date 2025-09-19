6647f841f1ce980243da6903f552952a8cb59cbe
Mike Wyatt <wyatt.mike@gmail.com>
2025-09-19 00:44:48 -0300

    ???

--- src/nvim/init.lua
+++ src/nvim/init.lua
@@ -25,7 +25,7 @@ require('lazy').setup({
   spec = vimrc.context.plugins,
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { 'oceanicnext' } },
+  install = { colorscheme = { 'base16-oceanicnext' } },
   -- automatically check for plugin updates
   -- checker = { enabled = true },
   ui = {
