cf72841ab0aefebfd039e45b2700f0b2e0e0bfaf
Mike Wyatt <wyatt.mike@gmail.com>
2025-09-28 18:52:10 -0300

    restore buffer arg

--- src/nvim/lua/features/scm.lua
+++ src/nvim/lua/features/scm.lua
@@ -22,41 +22,86 @@ mod.setup = vimrc.make_setup(function(context)
           -- visual mode
           map('stage git hunk', '<leader>hs', 'v', function()
             gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-          end)
+          end, { buffer = bufnr })
           map('reset git hunk', '<leader>hr', 'v', function()
             gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-          end)
+          end, { buffer = bufnr })
 
           -- normal mode
-          map('git [s]tage hunk', '<leader>hs', 'n', gitsigns.stage_hunk)
-          map('git [r]eset hunk', '<leader>hr', 'n', gitsigns.reset_hunk)
-          map('git [S]tage buffer', '<leader>hS', 'n', gitsigns.stage_buffer)
+          map(
+            'git [s]tage hunk',
+            '<leader>hs',
+            'n',
+            gitsigns.stage_hunk,
+            { buffer = bufnr }
+          )
+          map(
+            'git [r]eset hunk',
+            '<leader>hr',
+            'n',
+            gitsigns.reset_hunk,
+            { buffer = bufnr }
+          )
+          map(
+            'git [S]tage buffer',
+            '<leader>hS',
+            'n',
+            gitsigns.stage_buffer,
+            { buffer = bufnr }
+          )
           map(
             'git [u]ndo stage hunk',
             '<leader>hu',
             'n',
-            gitsigns.undo_stage_hunk
+            gitsigns.stage_hunk,
+            { buffer = bufnr }
           )
-          map('git [R]eset buffer', '<leader>hR', 'n', gitsigns.reset_buffer)
-          map('git [p]review hunk', '<leader>hp', 'n', gitsigns.preview_hunk)
-          map('git [b]lame line', '<leader>hb', 'n', gitsigns.blame_line)
-          map('git [d]iff against index', '<leader>hd', 'n', gitsigns.diffthis)
+          map(
+            'git [R]eset buffer',
+            '<leader>hR',
+            'n',
+            gitsigns.reset_buffer,
+            { buffer = bufnr }
+          )
+          map(
+            'git [p]review hunk',
+            '<leader>hp',
+            'n',
+            gitsigns.preview_hunk,
+            { buffer = bufnr }
+          )
+          map(
+            'git [b]lame line',
+            '<leader>hb',
+            'n',
+            gitsigns.blame_line,
+            { buffer = bufnr }
+          )
+          map(
+            'git [d]iff against index',
+            '<leader>hd',
+            'n',
+            gitsigns.diffthis,
+            { buffer = bufnr }
+          )
           map('git [D]iff against last commit', '<leader>hD', 'n', function()
             gitsigns.diffthis('@')
-          end)
+          end, { buffer = bufnr })
 
           -- Toggles
           map(
             '[T]oggle git show [b]lame line',
             '<leader>tb',
             'n',
-            gitsigns.toggle_current_line_blame
+            gitsigns.toggle_current_line_blame,
+            { buffer = bufnr }
           )
           map(
             '[T]oggle git show [D]eleted',
             '<leader>tD',
             'n',
-            gitsigns.preview_hunk_inline
+            gitsigns.preview_hunk_inline,
+            { buffer = bufnr }
           )
         end,
       },
